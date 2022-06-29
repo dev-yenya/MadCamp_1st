613bcc50efa72de6c013ccbed9adc16e4153b3c3
yen777 <parkyena01@sookmyung.ac.kr>
2022-06-29 19:55:03 +0900

    Initial commit

---
+++ app/src/main/java/com/example/first_app/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.first_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 하단 탭이 눌렸을 때 화면을 전환하기 위해선 이벤트 처리하기 위해 BottomNavigationView 객체 생성
+        var bnv_main = findViewById(R.id.bnv_main) as BottomNavigationView
+
+        // OnNavigationItemSelectedListener를 통해 탭 아이템 선택 시 이벤트를 처리
+        // navi_menu.xml 에서 설정했던 각 아이템들의 id를 통해 알맞은 프래그먼트로 변경하게 한다.
+        bnv_main.run { setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.first -> {
+                    // 다른 프래그먼트 화면으로 이동하는 기능
+                    val phoneBookFragment = PhoneBookFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, phoneBookFragment).commit()
+                }
+                R.id.second -> {
+                    val galleryFragment = GalleryFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, galleryFragment).commit()
+                }
+                R.id.third -> {
+                    val freeFragment = FreeFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, freeFragment).commit()
+                }
+            }
+            true
+        }
+            selectedItemId = R.id.first
+        }
+    }
+}
