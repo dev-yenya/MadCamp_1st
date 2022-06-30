fb8f81f1dba0b39c920e54d45c909e0a466107e1
yen777 <parkyena01@sookmyung.ac.kr>
2022-06-30 13:29:44 +0900

    갤러리 접근 권한 허용

--- app/src/main/java/com/example/first_app/GalleryFragment.kt
+++ app/src/main/java/com/example/first_app/GalleryFragment.kt
@@ -1,10 +1,17 @@
 package com.example.first_app
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.first_app.databinding.FragmentGalleryBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,20 +28,56 @@ class GalleryFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    val REQUEST_GET_IMAGE = 105
+    val PERMISSIONS_REQUEST_CODE = 100
+    var REQUIRED_PERMISSIONS = arrayOf<String>(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    //바인딩 객체 타입에 ?를 붙여서 null 허용(ondestory될 때 완벽하게 destroy 하기 위해)
+    private var mBinding: FragmentGalleryBinding?= null
+    //매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재선언
+    private val binding get() = mBinding!!
+
+
+   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false)
+        mBinding = FragmentGalleryBinding.inflate(inflater, container, false)
+
+        //갤러리 버튼이 클릭되면 갤러리를 연다
+        binding.btnGallery.setOnClickListener{
+            var intent = Intent(Intent.ACTION_PICK)
+            intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            intent.action = Intent.ACTION_GET_CONTENT
+            //startActivityForResult가 삭제! -> 대체 registerForActivtyResult()
+            startActivity(intent)
+        }
+
+        //val layoutManager = LinearLayoutManager(this)
+        //binding.recyclerView.layoutManager = layoutManager
+        //binding.recyclerView.adapter = adapter
+
+        return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        //onDestroyView 에서 binding class 인스턴스 참조를 정리해주어야 한다.
+        mBinding = null
+        super.onDestroyView()
     }
 
     companion object {
