3cebb4425b4230f7c48d2cbba814e4b4807f6e13
yen777 <parkyena01@sookmyung.ac.kr>
2022-07-01 21:31:12 +0900

    Splash 추가

---
+++ app/src/main/java/com/example/first_app/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.first_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+    override fun onBackPressed(){
+        super.onBackPressed()
+    }
+}
